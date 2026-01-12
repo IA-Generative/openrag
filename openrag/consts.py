f84eac55ef0d1b32dbbff1a3b8fce9546dfa981e
Ahmath-Gadji <ahmathgadji27@gmail.com>
2026-01-12 13:17:40 +0000

    correct IndexError caused by empty list of chunks when performing chunking

--- openrag/consts.py
+++ openrag/consts.py
@@ -1,4 +1,8 @@
-PARTITION_PREFIX="openrag-"
-LEGACY_PARTITION_PREFIX="ragondin-"
+PARTITION_PREFIX = "openrag-"
+LEGACY_PARTITION_PREFIX = "ragondin-"
 
-FILE_READ_CHUNK_SIZE = 1024 * 1024 # Read file in blocks of 1MB to preserve RAM
+FILE_READ_CHUNK_SIZE = 1024 * 1024  # Read file in blocks of 1MB to preserve RAM
+
+IMAGE_PLACEHOLDER = (
+    """<image_description>\n\n[Image Placeholder]\n\n</image_description>"""
+)
