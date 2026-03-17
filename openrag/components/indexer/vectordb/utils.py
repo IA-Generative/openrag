b7e9d9ec27cfae0de9aeff112ef0d8f61f064c4a
Ahmath-Gadji <ahmathgadji27@gmail.com>
2026-03-17 14:19:21 +0000

    test(relationships): add test for standalone file yielding no extra chunks on expansion

--- openrag/components/indexer/vectordb/utils.py
+++ openrag/components/indexer/vectordb/utils.py
@@ -548,6 +548,7 @@ class PartitionFileManager:
                         relationship_id, 0 as depth
                     FROM files
                     WHERE file_id = :file_id AND partition_name = :partition
+                        AND relationship_id IS NOT NULL
 
                     UNION ALL
 
@@ -557,6 +558,7 @@ class PartitionFileManager:
                     FROM files f
                     INNER JOIN ancestors a ON f.file_id = a.parent_id
                         AND f.partition_name = a.partition_name
+                        AND f.relationship_id IS NOT NULL
                     {depth_condition}
                 )
                 SELECT * FROM ancestors ORDER BY depth DESC
